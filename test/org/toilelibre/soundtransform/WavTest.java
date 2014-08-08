60364b73fad3e9a49ee879f31782e0fe5dd2100e
libetl <libe4@free.fr>
2014-08-08 22:09:29 +0200

    junit bugfix

--- test/org/toilelibre/soundtransform/WavTest.java
+++ test/org/toilelibre/soundtransform/WavTest.java
@@ -8,17 +8,48 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import org.junit.Test;
 import org.toilelibre.soundtransform.observer.PrintlnTransformObserver;
 import org.toilelibre.soundtransform.transforms.EightBitsSoundTransformation;
+import org.toilelibre.soundtransform.transforms.EqualizerSoundTransformation;
+import org.toilelibre.soundtransform.transforms.ReverseSoundTransformation;
 
 public class WavTest {
 
+	private ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+	private File input  = new File (classLoader.getResource("before.wav").getFile());
+	private File output = new File (new File (
+			classLoader.getResource("before.wav").getFile()).getParent() + "/after.wav");
+	
 	@Test
 	public void test8bits (){
 		try {
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-			new TransformSound(new PrintlnTransformObserver()).transformWav(
-					new File (classLoader.getResource("before.wav").getFile()), 
-					new File (classLoader.getResource("before.wav").getPath() + "after.wav"), 
+			new TransformSound(new PrintlnTransformObserver()).transformWav (input, output,
 					new EightBitsSoundTransformation(25));
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	@Test
+	public void testRemoveLowFreqs (){
+		try {
+			new TransformSound(new PrintlnTransformObserver()).transformWav (input, output,
+			    new EqualizerSoundTransformation(
+				  new double [] {0, 2000, 4000, 6000, 8000, 10000, 12000, 14000, 16000, 18000, 22050},
+				  new double [] {0,    0,  0.1,  0.3,  0.7,     1,     1,     1,     1,     1,     1}));
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testReverse (){
+		try {
+			new TransformSound(new PrintlnTransformObserver()).transformWav (input, output,
+			    new ReverseSoundTransformation());
 		} catch (UnsupportedAudioFileException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
