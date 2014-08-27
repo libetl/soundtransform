70b0bb6a5f8a8d4d9f2c6bde56d3d2514f0c652a
libetl <libe4@free.fr>
2014-08-27 23:16:33 +0200

    Format

--- test/org/toilelibre/soundtransform/Sound2NoteTest.java
+++ test/org/toilelibre/soundtransform/Sound2NoteTest.java
@@ -10,19 +10,17 @@ import org.junit.Test;
 import org.toilelibre.soundtransform.objects.Note;
 
 public class Sound2NoteTest {
-    private ClassLoader classLoader = Thread.currentThread()
-            .getContextClassLoader();
-    private File input = new File(classLoader.getResource("piano_a.wav")
-            .getFile());
+	private ClassLoader	classLoader	= Thread.currentThread ().getContextClassLoader ();
+	private File	    input	    = new File (classLoader.getResource ("piano_a.wav").getFile ());
 
-    @Test
-    public void run() throws UnsupportedAudioFileException, IOException {
+	@Test
+	public void run () throws UnsupportedAudioFileException, IOException {
 
-        AudioInputStream ais = AudioFileHelper.getAudioInputStream(input);
-        TransformSound ts = new TransformSound();
+		AudioInputStream ais = AudioFileHelper.getAudioInputStream (input);
+		TransformSound ts = new TransformSound ();
 
-        Note n =  Sound2Note.convert(ts.fromInputStream(ais));
+		Note n = Sound2Note.convert (ts.fromInputStream (ais));
 
-    }
-    
+	}
+
 }
