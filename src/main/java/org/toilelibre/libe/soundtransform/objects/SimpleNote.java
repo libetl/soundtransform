978b70924bd539da7ce9ef5999d9be923d21515f
Lionel Benychou <lionel.benychou@sage.com>
2014-09-02 18:07:33 +0200

    Bugfix

--- src/main/java/org/toilelibre/libe/soundtransform/objects/SimpleNote.java
+++ src/main/java/org/toilelibre/libe/soundtransform/objects/SimpleNote.java
@@ -27,8 +27,9 @@ public class SimpleNote implements Note {
 	}
 
 	private float getRatio (Sound [] subsound) {
-		return subsound [0].getSamples ().length * 1.0f
-		        / (this.attack [0].getSamples ().length + this.decay [0].getSamples ().length + this.sustain [0].getSamples ().length + this.release [0].getSamples ().length);
+	    int lengthOfSubsound = subsound [0].getSamples ().length;
+	    int lengthOfSound = (this.attack [0].getSamples ().length + this.decay [0].getSamples ().length + this.sustain [0].getSamples ().length + this.release [0].getSamples ().length);
+		return lengthOfSubsound * 1.0f / lengthOfSound;
 	}
 
 	@Override
@@ -65,7 +66,7 @@ public class SimpleNote implements Note {
 		if (percent < 98 || percent > 102) {
 			result = pitcher.transform (result);
 		}
-		double factor = subSound [0].getSamples().length == 0 ? 0 : length * 1.0 / subSound [0].getSamples().length;
+		double factor = subSound [0].getSamples().length == 0 ? 0 : length * 1.0 / result.getSamples().length;
 		if (factor == 0){
 			return result;
 		} else if (factor < 0.98 || factor > 1.02) {
