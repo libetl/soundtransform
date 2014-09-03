8cc6efb407e31fdd9b49eb1b8b1e4a9a3a8516ba
libetl <libe4@free.fr>
2014-09-03 23:22:00 +0200

    FrequenciesState toString is a plot

--- src/main/java/org/toilelibre/libe/soundtransform/objects/FrequenciesState.java
+++ src/main/java/org/toilelibre/libe/soundtransform/objects/FrequenciesState.java
@@ -20,5 +20,54 @@ public class FrequenciesState {
 	public int getMaxfrequency () {
 		return maxfrequency;
 	}
+	
+	public String toString (){
+		float lastFrequency = maxfrequency / 2.0f;
+		int length = (int) lastFrequency / 100;
+		int height = 15;
+		int maxMagn = 256*256*16;
+		StringBuffer sb = new StringBuffer ();
+		int step = (int) lastFrequency / length;
+		int [] valuesOnPlot = new int [length];
+		int max = 0;
+		int maxValue = 0;
+		for (int i = 0 ; i < valuesOnPlot.length ; i++){
+			double sum = 0;
+			for (int j = 0 ; j < step ; j++){
+				sum += state [i * step + j].abs ();
+			}
+			valuesOnPlot [i] = (int)(sum * 1.0 / step * height / (maxMagn));
+			if (maxValue < valuesOnPlot [i]){
+				maxValue = valuesOnPlot [i];
+				max = i;
+			}
+		}
+		for (int j = height ; j >= 0 ; j--){
+			if (j == height){
+				sb.append ("^ " + maxMagn + " (magnitude)\n");
+				continue;
+			}else{
+				sb.append ("|");
+			}
+			for (int i = 0 ; i < length ; i++){
+				if (valuesOnPlot [i] == j){
+					sb.append ("_");
+				}else if (valuesOnPlot [i] > j){
+					sb.append ("#");
+				}else{
+					sb.append (" ");
+				}
+			}
+			sb.append ("\n");
+		}
+		sb.append ("L");
+		for (int i = 0 ; i < length; i++){
+			sb.append ("-");
+		}
+		sb.append ("> " + lastFrequency + "Hz (freq)");
+		sb.append ("\nMax is in the range "  + 
+		(int)(max * 1.0 / length * lastFrequency) + "Hz - " + (int)((max + 1.0) / length * lastFrequency) + "Hz");
+		return sb.toString ();
+	}
 
 }
