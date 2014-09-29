ed69c2b00ae886f73cdfe912ff9ff0ac7c19b3fd
libetl <libe4@free.fr>
2014-09-29 23:17:55 +0200

    Better junit

--- src/test/java/org/toilelibre/libe/soundtransform/Sound2NoteTest.java
+++ src/test/java/org/toilelibre/libe/soundtransform/Sound2NoteTest.java
@@ -18,95 +18,82 @@ import org.toilelibre.libe.soundtransform.pda.Sound2Note;
 
 public class Sound2NoteTest {
 
-    @Test
-    public void run() throws UnsupportedAudioFileException, IOException {
-        @SuppressWarnings("serial")
-        Map<String, Integer> frequenciesPerSound = new HashMap<String, Integer>() {
-            {
-                this.put("Piano1-C.wav", 260);// OK
-                this.put("Piano2-D.wav", 293);// OK
-                this.put("Piano3-E.wav", 332);// OK
-                this.put("Piano4-F.wav", 344);// OK
-                this.put("Piano5-G.wav", 376);// OK
-                this.put("Piano6-A.wav", 451);// OK
-                this.put("Piano7-B.wav", 499);// OK
-                this.put("Piano8-C.wav", 524);// OK
-            }
-        };
-        System.out.println("Loading Packs");
-        Pack pack = PacksList.getInstance().defaultPack;
-        for (String instrument : pack.keySet()) {
-            for (Integer noteKey : pack.get(instrument).keySet()) {
-                Note n = pack.get(instrument).get(noteKey);
-                if (frequenciesPerSound.get(n.getName()) != null) {
-                    org.junit.Assert.assertEquals(
-                            frequenciesPerSound.get(n.getName()).intValue(),
-                            n.getFrequency());
-                    System.out.println("f0 (" + n.getName() + ") = "
-                            + n.getFrequency());
-                } else {
-                    System.out.println("Did not find " + n.getName());
-                }
-            }
-        }
-    }
+	@Test
+	public void run () throws UnsupportedAudioFileException, IOException {
+		@SuppressWarnings ("serial")
+		Map<String, Integer> frequenciesPerSound = new HashMap<String, Integer> () {
+			{
+				this.put ("Piano1-C.wav", 260);// OK
+				this.put ("Piano2-D.wav", 293);// OK
+				this.put ("Piano3-E.wav", 332);// OK
+				this.put ("Piano4-F.wav", 344);// OK
+				this.put ("Piano5-G.wav", 376);// OK
+				this.put ("Piano6-A.wav", 451);// OK
+				this.put ("Piano7-B.wav", 499);// OK
+				this.put ("Piano8-C.wav", 524);// OK
+			}
+		};
+		System.out.println ("Loading Packs");
+		Pack pack = PacksList.getInstance ().defaultPack;
+		for (String instrument : pack.keySet ()) {
+			for (Integer noteKey : pack.get (instrument).keySet ()) {
+				Note n = pack.get (instrument).get (noteKey);
+				if (frequenciesPerSound.get (n.getName ()) != null) {
+					org.junit.Assert.assertEquals (frequenciesPerSound.get (n.getName ()).intValue (), n.getFrequency ());
+					System.out.println ("f0 (" + n.getName () + ") = " + n.getFrequency ());
+				} else {
+					System.out.println ("Did not find " + n.getName ());
+				}
+			}
+		}
+	}
 
-    @Test
-    public void shouldNotBeTwiceTheF0ValuePiano1C()
-            throws UnsupportedAudioFileException, IOException {
-        ClassLoader classLoader = Sound2NoteTest.class.getClassLoader();
-        URL fileURL = classLoader.getResource("notes/Piano1-C.wav");
-        File input = new File(fileURL.getFile());
+	@Test
+	public void shouldNotBeTwiceTheF0ValuePiano1C () throws UnsupportedAudioFileException, IOException {
+		ClassLoader classLoader = Sound2NoteTest.class.getClassLoader ();
+		URL fileURL = classLoader.getResource ("notes/Piano1-C.wav");
+		File input = new File (fileURL.getFile ());
 
-        AudioInputStream ais = AudioFileHelper.getAudioInputStream(input);
-        TransformSound ts = new TransformSound();
+		AudioInputStream ais = AudioFileHelper.getAudioInputStream (input);
+		TransformSound ts = new TransformSound ();
 
-        Note n = Sound2Note.convert("Piano1-C.wav", ts.fromInputStream(ais));
-        System.out.println("c' 1-line octave : " + n.getFrequency() + "Hz, should be around 261Hz");
-        org.junit.Assert.assertTrue(n.getFrequency() > 261 - 10
-                && n.getFrequency() < 261 + 10);
-    }
-    
+		Note n = Sound2Note.convert ("Piano1-C.wav", ts.fromInputStream (ais));
+		System.out.println ("c' 1-line octave : " + n.getFrequency () + "Hz, should be around 261Hz");
+		org.junit.Assert.assertTrue (n.getFrequency () > 261 - 10 && n.getFrequency () < 261 + 10);
+	}
 
-    @Test
-    public void shouldNotBeTwiceTheF0ValuePiano4F()
-            throws UnsupportedAudioFileException, IOException {
-        ClassLoader classLoader = Sound2NoteTest.class.getClassLoader();
-        URL fileURL = classLoader.getResource("notes/Piano4-F.wav");
-        File input = new File(fileURL.getFile());
+	@Test
+	public void shouldNotBeTwiceTheF0ValuePiano4F () throws UnsupportedAudioFileException, IOException {
+		ClassLoader classLoader = Sound2NoteTest.class.getClassLoader ();
+		URL fileURL = classLoader.getResource ("notes/Piano4-F.wav");
+		File input = new File (fileURL.getFile ());
 
-        AudioInputStream ais = AudioFileHelper.getAudioInputStream(input);
-        TransformSound ts = new TransformSound();
+		AudioInputStream ais = AudioFileHelper.getAudioInputStream (input);
+		TransformSound ts = new TransformSound ();
 
-        Note n = Sound2Note.convert("Piano4-F.wav", ts.fromInputStream(ais));
-        System.out.println("f' 4 : " + n.getFrequency() + "Hz, should be around 349Hz");
-        org.junit.Assert.assertTrue(n.getFrequency() > 349 - 10
-                && n.getFrequency() < 349 + 10);
-    }
-    
-    @Test
-    public void shouldRecognize440HzNote(){
-    	long [] signal = new long [4410];
-    	for (int i = 0 ; i < 4410 ; i++){
-    		signal [i] = (long) (Math.sin(i / 4) * 256 * 128.0);
-    	}
-    	Sound s = new Sound (signal, 2, 44100, 1);
-    	Note n = Sound2Note.convert("Sample 440Hz Sound", new Sound []{s});
-        System.out.println("Sample 440Hz Sound : " + n.getFrequency() + "Hz, should be around 440Hz");
-    	org.junit.Assert.assertTrue(n.getFrequency() > 440 - 10
-                && n.getFrequency() < 440 + 10);
-    }
-    
-    @Test
-    public void shouldRecognize529HzNote(){
-    	long [] signal = new long [4410];
-    	for (int i = 0 ; i < 4410 ; i++){
-    		signal [i] = (long) (Math.sin(i / 3.318181) * 256 * 128.0);
-    	}
-    	Sound s = new Sound (signal, 2, 44100, 1);
-    	Note n = Sound2Note.convert("Sample 529Hz Sound", new Sound []{s});
-        System.out.println("Sample 529Hz Sound : " + n.getFrequency() + "Hz, should be around 529Hz");
-    	org.junit.Assert.assertTrue(n.getFrequency() > 529 - 10
-                && n.getFrequency() < 529 + 10);
-    }
+		Note n = Sound2Note.convert ("Piano4-F.wav", ts.fromInputStream (ais));
+		System.out.println ("f' 4 : " + n.getFrequency () + "Hz, should be around 349Hz");
+		org.junit.Assert.assertTrue (n.getFrequency () > 349 - 10 && n.getFrequency () < 349 + 10);
+	}
+
+	@Test
+	public void shouldRecognizeSimpleNotes () {
+		int length = 4410;
+		int [] notes = new int [] { 261, 293, 329, 349, 392, 440, 493 };
+		String [] notesTitle = new String [] { "C4", "D4", "E4", "F4", "G4", "A4", "B4" };
+
+		for (int i = 0; i < notes.length; i++) {
+			int samplerate = 44100;
+			long [] signal = new long [length];
+			for (int j = 0; j < length; j++) {
+				signal [j] = (long) (Math.sin (j * notes [i] * 25.0 / samplerate) * 256 * 128.0);
+			}
+			Sound s = new Sound (signal, 2, samplerate, 1);
+			Note n = Sound2Note.convert ("Sample " + notesTitle [i] + "(" + notes [i] + "Hz) Sound", new Sound [] { s });
+			System.out.println ("Sample " + notesTitle [i] + "(" + notes [i] + "Hz) Sound, but frequency found was " + n.getFrequency () + "Hz");
+			org.junit.Assert.assertTrue (n.getFrequency () > notes [i] - 10 && n.getFrequency () < notes [i] + 10);
+			System.out.println ("...acceptable");
+		}
+	}
+
 }
