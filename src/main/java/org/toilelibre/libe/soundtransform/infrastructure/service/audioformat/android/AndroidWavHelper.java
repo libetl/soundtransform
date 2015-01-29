c6b873e755bfd30adc1e7c845b7d41e222fd46c8
Lionel Benychou <lionel.benychou@sage.com>
2015-01-29 10:00:53 +0100

    Fixed a little bug when there is no info in the wav sound

--- src/main/java/org/toilelibre/libe/soundtransform/infrastructure/service/audioformat/android/AndroidWavHelper.java
+++ src/main/java/org/toilelibre/libe/soundtransform/infrastructure/service/audioformat/android/AndroidWavHelper.java
@@ -84,7 +84,8 @@ public class AndroidWavHelper {
 
     public void writeMetadata (ByteArrayWithAudioFormatInputStream audioInputStream, WavOutputStream outputStream) throws IOException {
         final InputStreamInfo info = audioInputStream.getInfo ();
-        final int fileSize = (int) (AndroidWavHelper.INFO_METADATA_SIZE + info.getSoundInfo ().length () +
+        final int soundInfoSize = info.getSoundInfo () == null ? 0 : info.getSoundInfo ().length ();
+        final int fileSize = (int) (AndroidWavHelper.INFO_METADATA_SIZE + soundInfoSize +
                                     (info.getFrameLength () * info.getSampleSize () * info.getChannels ()));
         final int chunkSize = AndroidWavHelper.INFO_CHUNK_SIZE;
         final int typeOfEncoding = 1;
