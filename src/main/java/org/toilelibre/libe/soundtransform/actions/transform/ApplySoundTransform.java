cbff0b015ed2bf93b807f49544ec53054c4f55f9
libetl <libe4@free.fr>
2015-02-01 23:13:40 +0100

    Removed useless action method

--- src/main/java/org/toilelibre/libe/soundtransform/actions/transform/ApplySoundTransform.java
+++ src/main/java/org/toilelibre/libe/soundtransform/actions/transform/ApplySoundTransform.java
@@ -1,7 +1,5 @@
 package org.toilelibre.libe.soundtransform.actions.transform;
 
-import java.io.InputStream;
-
 import org.toilelibre.libe.soundtransform.actions.Action;
 import org.toilelibre.libe.soundtransform.model.converted.SoundTransformation;
 import org.toilelibre.libe.soundtransform.model.converted.sound.Sound;
@@ -11,9 +9,5 @@ public final class ApplySoundTransform extends Action {
 
     public Sound [] apply (final Sound [] sounds, final SoundTransformation transform) throws SoundTransformException {
         return this.transformSound.apply (sounds, transform);
-    }
-
-    public Sound [] convertAndApply (final InputStream ais, final SoundTransformation transform) throws SoundTransformException {
-        return this.transformSound.convertAndApply (ais, transform);
     }
 }
