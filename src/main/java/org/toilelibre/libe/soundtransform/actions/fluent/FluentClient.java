362583847cbca30f5a1002e5e87629ab6fe61f38
Lionel Benychou <lionel.benychou@sage.com>
2015-03-09 15:44:11 +0100

    Sonar drilldown

--- src/main/java/org/toilelibre/libe/soundtransform/actions/fluent/FluentClient.java
+++ src/main/java/org/toilelibre/libe/soundtransform/actions/fluent/FluentClient.java
@@ -87,7 +87,7 @@ public class FluentClient implements FluentClientSoundImported, FluentClientRead
      * @return the client, in its current state.
      */
     public static void setDefaultObservers (final Observer... defaultObservers1) {
-        final LinkedList<Observer> newValue = new LinkedList<Observer> ();
+        final List<Observer> newValue = new LinkedList<Observer> ();
         newValue.addAll (Arrays.<Observer>asList (defaultObservers1));
         FluentClient.defaultObservers = newValue;
     }
