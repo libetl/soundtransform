a037bd2ba7aec5faf5914489547b1c146faffcc5
libetl <libe4@free.fr>
2015-03-11 21:04:21 +0100

    Faster ADSRHelper, but maybe not optimal

--- src/main/java/org/toilelibre/libe/soundtransform/infrastructure/service/sound2note/MagnitudeADSRHelper.java
+++ src/main/java/org/toilelibre/libe/soundtransform/infrastructure/service/sound2note/MagnitudeADSRHelper.java
@@ -1,10 +1,12 @@
 package org.toilelibre.libe.soundtransform.infrastructure.service.sound2note;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.apache.commons.math3.exception.NonMonotonicSequenceException;
 import org.apache.commons.math3.util.MathArrays;
 import org.toilelibre.libe.soundtransform.infrastructure.service.converted.sound.transforms.ADSREnveloppeSoundTransformation;
 import org.toilelibre.libe.soundtransform.model.converted.sound.Sound;
-import org.toilelibre.libe.soundtransform.model.converted.sound.transform.ReverseSoundTransformation;
 import org.toilelibre.libe.soundtransform.model.exception.SoundTransformException;
 import org.toilelibre.libe.soundtransform.model.library.note.ADSRHelper;
 import org.toilelibre.libe.soundtransform.model.observer.AbstractLogAware;
@@ -38,11 +40,12 @@ public class MagnitudeADSRHelper extends AbstractLogAware<MagnitudeADSRHelper> i
 
     private static final int ACCURATE_STEP_FOR_ADSR_HELPER = 100;
 
+    private double [] magnitude = null;
+
     @Override
     public int findDecay (final Sound channel1, final int attack) throws SoundTransformException {
         int decayIndex = attack;
-
-        final double [] magnitude = this.getMagnitudeArray (channel1, MagnitudeADSRHelper.ACCURATE_STEP_FOR_ADSR_HELPER);
+        this.ensureComputedMagnitudeArray (channel1, MagnitudeADSRHelper.ACCURATE_STEP_FOR_ADSR_HELPER);
 
         final double [] decayArray = new double [magnitude.length - attack];
         System.arraycopy (magnitude, attack, decayArray, 0, magnitude.length - attack);
@@ -57,11 +60,12 @@ public class MagnitudeADSRHelper extends AbstractLogAware<MagnitudeADSRHelper> i
 
     @Override
     public int findRelease (final Sound channel1) throws SoundTransformException {
-        final Sound reversed = new ReverseSoundTransformation ().transform (channel1);
         int releaseIndexFromReversed = 0;
-
-        final double [] magnitude = this.getMagnitudeArray (reversed, MagnitudeADSRHelper.ACCURATE_STEP_FOR_ADSR_HELPER);
-
+        this.ensureComputedMagnitudeArray (channel1, MagnitudeADSRHelper.ACCURATE_STEP_FOR_ADSR_HELPER);
+        final double [] reversed = new double [this.magnitude.length];
+        System.arraycopy (this.magnitude, 0, reversed, 0, reversed.length);
+        Collections.reverse (Arrays.asList (reversed));
+        
         try {
             MathArrays.checkOrder (magnitude, MathArrays.OrderDirection.INCREASING, true);
         } catch (final NonMonotonicSequenceException nmse) {
@@ -74,8 +78,7 @@ public class MagnitudeADSRHelper extends AbstractLogAware<MagnitudeADSRHelper> i
     @Override
     public int findSustain (final Sound channel1, final int decay) throws SoundTransformException {
         int sustainIndex = decay;
-
-        final double [] magnitude = this.getMagnitudeArray (channel1, MagnitudeADSRHelper.ACCURATE_STEP_FOR_ADSR_HELPER);
+        this.ensureComputedMagnitudeArray (channel1, MagnitudeADSRHelper.ACCURATE_STEP_FOR_ADSR_HELPER);
 
         final int start = decay / MagnitudeADSRHelper.ACCURATE_STEP_FOR_ADSR_HELPER;
         final double [] sustainArray = new double [magnitude.length - start];
@@ -89,10 +92,11 @@ public class MagnitudeADSRHelper extends AbstractLogAware<MagnitudeADSRHelper> i
         return sustainIndex;
     }
 
-    private double [] getMagnitudeArray (final Sound sound, final int step) {
-
-        final ADSREnveloppeSoundTransformation soundTransform = new ADSREnveloppeSoundTransformation (step);
-        soundTransform.transform (sound);
-        return soundTransform.getMagnitude ();
+    private void ensureComputedMagnitudeArray (final Sound sound, final int step) {
+        if (this.magnitude == null){
+            final ADSREnveloppeSoundTransformation soundTransform = new ADSREnveloppeSoundTransformation (step);
+            soundTransform.transform (sound);
+            this.magnitude =  soundTransform.getMagnitude ();
+        }
     }
 }
