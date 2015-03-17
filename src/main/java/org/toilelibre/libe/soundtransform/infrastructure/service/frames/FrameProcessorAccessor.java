ff702af127bc37b62a6a44c1da4831d2f701b804
libetl <libe4@free.fr>
2015-03-17 23:39:36 +0100

    Sonar defects

--- src/main/java/org/toilelibre/libe/soundtransform/infrastructure/service/frames/FrameProcessorAccessor.java
+++ src/main/java/org/toilelibre/libe/soundtransform/infrastructure/service/frames/FrameProcessorAccessor.java
@@ -2,10 +2,11 @@ package org.toilelibre.libe.soundtransform.infrastructure.service.frames;
 
 import org.toilelibre.libe.soundtransform.infrastructure.service.fourier.FourierAccessor;
 import org.toilelibre.libe.soundtransform.model.inputstream.FrameProcessor;
+import org.toilelibre.libe.soundtransform.model.observer.AbstractLogAware;
 
 public abstract class FrameProcessorAccessor extends FourierAccessor {
 
-    protected FrameProcessor<?> provideFrameProcessor() {
+    protected FrameProcessor<AbstractLogAware<ByteArrayFrameProcessor>> provideFrameProcessor() {
         return new ByteArrayFrameProcessor();
     }
 }
