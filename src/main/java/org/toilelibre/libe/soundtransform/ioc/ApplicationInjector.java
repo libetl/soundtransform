53e7b222c718ffabeb5c4f4ab4976395d2b34570
Lionel Benychou <lionel.benychou@sage.com>
2015-03-19 16:28:00 +0100

    Delegating the intelligence to Silk, the ApplicationInjector does not anymore construct itself a bean

--- src/main/java/org/toilelibre/libe/soundtransform/ioc/ApplicationInjector.java
+++ src/main/java/org/toilelibre/libe/soundtransform/ioc/ApplicationInjector.java
@@ -1,7 +1,6 @@
 package org.toilelibre.libe.soundtransform.ioc;
 
 import org.toilelibre.libe.soundtransform.ioc.android.AndroidRootModule;
-import org.toilelibre.libe.soundtransform.model.exception.ErrorCode;
 
 import se.jbee.inject.Dependency;
 import se.jbee.inject.Injector;
@@ -27,21 +26,6 @@ public class ApplicationInjector {
 
     }
 
-    public enum ApplicationInjectorErrorCode implements ErrorCode {
-
-        INSTANTIATION_FAILED ("Instantiation failed (seen warnings list : %1s)");
-
-        private final String messageFormat;
-
-        ApplicationInjectorErrorCode (final String mF) {
-            this.messageFormat = mF;
-        }
-
-        @Override
-        public String getMessageFormat () {
-            return this.messageFormat;
-        }
-    }
 
     public static <T> T getBean (final Class<T> type) {
         return ApplicationInjector.injector.resolve (Dependency.<T> dependency (type));
