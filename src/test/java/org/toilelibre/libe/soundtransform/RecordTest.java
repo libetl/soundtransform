2e2a9092a48dc95ba457e6b4ae1131078b147ebc
libetl <libe4@free.fr>
2015-03-20 22:30:17 +0100

    Record in Javax bugfix

--- src/test/java/org/toilelibre/libe/soundtransform/RecordTest.java
+++ src/test/java/org/toilelibre/libe/soundtransform/RecordTest.java
@@ -3,7 +3,6 @@ package org.toilelibre.libe.soundtransform;
 import org.junit.Test;
 import org.toilelibre.libe.soundtransform.actions.fluent.FluentClient;
 import org.toilelibre.libe.soundtransform.ioc.SoundTransformTest;
-import org.toilelibre.libe.soundtransform.model.converted.sound.Sound;
 import org.toilelibre.libe.soundtransform.model.exception.SoundTransformException;
 import org.toilelibre.libe.soundtransform.model.inputstream.StreamInfo;
 
@@ -11,7 +10,6 @@ public class RecordTest extends SoundTransformTest {
 
     @Test
     public void recordTwoSeconds () throws SoundTransformException{
-        Sound [] sounds = FluentClient.start().withLimitedTimeRecordedInputStream(new StreamInfo (2, 100000, 2, 48000, false, true, null)).importToSound().stopWithSounds();
-        sounds.hashCode ();
+        FluentClient.start().withLimitedTimeRecordedInputStream(new StreamInfo (2, 100000, 2, 48000, false, true, null)).writeToClasspathResourceWithSiblingResource ("after.wav", "before.wav");
     }
 }
