5e60504cb96feb63c7ffc0609223e126e32bcc4e
Lionel Benychou <lionel.benychou@sage.com>
2015-03-20 15:09:29 +0100

    Record audio, part 3

--- src/test/java/org/toilelibre/libe/soundtransform/RecordTest.java
+++ src/test/java/org/toilelibre/libe/soundtransform/RecordTest.java
@@ -2,10 +2,11 @@ package org.toilelibre.libe.soundtransform;
 
 import org.junit.Test;
 import org.toilelibre.libe.soundtransform.actions.fluent.FluentClient;
+import org.toilelibre.libe.soundtransform.ioc.SoundTransformTest;
 import org.toilelibre.libe.soundtransform.model.exception.SoundTransformException;
 import org.toilelibre.libe.soundtransform.model.inputstream.StreamInfo;
 
-public class RecordTest {
+public class RecordTest extends SoundTransformTest {
 
     @Test
     public void recordTwoSeconds () throws SoundTransformException{
