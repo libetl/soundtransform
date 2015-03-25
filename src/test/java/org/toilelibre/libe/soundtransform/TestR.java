58d77232c286eeb0e314fe550d2019f1a49485da
Lionel Benychou <lionel.benychou@sage.com>
2015-03-25 18:59:52 +0100

    Added a badidpack test

---
+++ src/test/java/org/toilelibre/libe/soundtransform/TestR.java
@@ -0,0 +1,9 @@
+package org.toilelibre.libe.soundtransform;
+
+public final class TestR {
+    public static final class attr {
+    }
+    public static final class raw {
+        public static int badidpack=0x7f020003;
+    }
+}
