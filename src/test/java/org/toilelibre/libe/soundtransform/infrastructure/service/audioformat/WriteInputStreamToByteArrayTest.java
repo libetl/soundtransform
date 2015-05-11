7de06e47ce705c944bdd75167b5e8d8395bc6f40
libetl <libe4@free.fr>
2015-05-11 23:13:13 +0200

    WriteInputStreamToByteArrayTest

--- src/test/java/org/toilelibre/libe/soundtransform/infrastructure/service/audioformat/WriteInputStreamToByteArrayTest.java
+++ src/test/java/org/toilelibre/libe/soundtransform/infrastructure/service/audioformat/WriteInputStreamToByteArrayTest.java
@@ -1,20 +1,30 @@
 package org.toilelibre.libe.soundtransform.infrastructure.service.audioformat;
 
-import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.toilelibre.libe.soundtransform.model.exception.SoundTransformException;
 
 public class WriteInputStreamToByteArrayTest {
 
-    @Test
+    @Test (expected = SoundTransformException.class)
     public void errorWhileReadingStream () throws SoundTransformException {
         try {
             final WriteInputStreamToByteArray writer = new WriteInputStreamToByteArray ();
-            writer.convertToByteArray (new ByteArrayInputStream (new byte [1]));
+            InputStream failingInputStream = Mockito.mock (InputStream.class);
+            try {
+                Mockito.when (failingInputStream.read (Mockito.any (byte [].class), Mockito.any (int.class), Mockito.any (int.class))).thenThrow (
+                        new IOException ("Forced failure"));
+            } catch (IOException e) {
+                throw new RuntimeException (e);
+            }
+            writer.convertToByteArray (failingInputStream);
         } catch (final SoundTransformException ste) {
             Assert.assertEquals (ste.getErrorCode (), WriteInputStreamToByteArray.WriteInputStreamToByteArrayErrorCode.ERROR_WHILE_READING_STREAM);
+            throw ste;
         }
     }
 }
