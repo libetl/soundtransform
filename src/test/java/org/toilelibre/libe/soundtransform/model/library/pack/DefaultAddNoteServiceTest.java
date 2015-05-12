278cc65713dcd7a21187eb1509350a450549f7f5
libetl <libe4@free.fr>
2015-05-12 23:25:05 +0200

    DefaultAddNoteServiceTest

---
+++ src/test/java/org/toilelibre/libe/soundtransform/model/library/pack/DefaultAddNoteServiceTest.java
@@ -0,0 +1,17 @@
+package org.toilelibre.libe.soundtransform.model.library.pack;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
+import org.toilelibre.libe.soundtransform.ioc.ApplicationInjector.$;
+import org.toilelibre.libe.soundtransform.model.exception.SoundTransformException;
+
+public class DefaultAddNoteServiceTest {
+
+    @Test (expected = SoundTransformException.class)
+    public void addNoteWithInvalidInputStream () throws SoundTransformException, UnsupportedEncodingException{
+         $.select (AddNoteService.class).addNote (new Range (), new SimpleNoteInfo (""), new ByteArrayInputStream ("dfsqfdsqfdsqdfdfqsqsdf".getBytes ("UTF-8")));
+    }
+}
