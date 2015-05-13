99347a7ade0b2685b8f4e680af9a5c826f9a6694
libetl <libe4@free.fr>
2015-05-13 22:59:45 +0200

    ByteArrayFrameProcessorTest

---
+++ src/test/java/org/toilelibre/libe/soundtransform/infrastructure/service/frames/ByteArrayFrameProcessorTest.java
@@ -0,0 +1,49 @@
+package org.toilelibre.libe.soundtransform.infrastructure.service.frames;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.toilelibre.libe.soundtransform.model.exception.SoundTransformException;
+import org.toilelibre.libe.soundtransform.model.inputstream.FrameProcessor.FrameProcessorErrorCode;
+import org.toilelibre.libe.soundtransform.model.inputstream.StreamInfo;
+
+public class ByteArrayFrameProcessorTest {
+
+    @Test (expected = SoundTransformException.class)
+    public void simulateAvailableFailure () throws IOException, SoundTransformException{
+        InputStream ais = Mockito.mock (InputStream.class);
+        Mockito.when (ais.available ()).thenThrow (new IOException ("Could not open stream"));
+        try {
+            new ByteArrayFrameProcessor ().fromInputStream (ais, new StreamInfo (2, -1, 2, 441000.0f, false, true, null));
+        } catch (SoundTransformException ste){
+            Assert.assertEquals (FrameProcessorErrorCode.COULD_NOT_FIND_LENGTH, ste.getErrorCode ());
+            throw ste;
+        }
+    }
+    
+    @Test (expected = SoundTransformException.class)
+    public void simulateCloseFailure () throws IOException, SoundTransformException{
+        InputStream ais = Mockito.mock (InputStream.class);
+        Mockito.doThrow (new IOException ("Could not close stream")).when (ais).close ();
+        try {
+            new ByteArrayFrameProcessor ().fromInputStream (ais, new StreamInfo (2, 200000, 2, 441000.0f, false, true, null));
+        } catch (SoundTransformException ste){
+            Assert.assertEquals (FrameProcessorErrorCode.COULD_NOT_CLOSE_STREAM, ste.getErrorCode ());
+            throw ste;
+        }
+    }
+    @Test (expected = SoundTransformException.class)
+    public void simulateReadFailure () throws IOException, SoundTransformException{
+        InputStream ais = Mockito.mock (InputStream.class);
+        Mockito.when (ais.read (Mockito.any (byte [].class))).thenThrow (new IOException ("Could not read stream"));
+        try {
+            new ByteArrayFrameProcessor ().fromInputStream (ais, new StreamInfo (2, 200000, 2, 441000.0f, false, true, null));
+        } catch (SoundTransformException ste){
+            Assert.assertEquals (FrameProcessorErrorCode.COULD_NOT_READ_STREAM, ste.getErrorCode ());
+            throw ste;
+        }
+    }
+}
