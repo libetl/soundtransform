a083d22574887555db24e1a09f5af2a460e1df6a
Lionel Benychou <lionel.benychou@sage.com>
2015-05-19 17:51:25 +0200

    Wrong error code used

--- src/main/java/org/toilelibre/libe/soundtransform/infrastructure/service/freqs/SimpleFilterFrequenciesProcessor.java
+++ src/main/java/org/toilelibre/libe/soundtransform/infrastructure/service/freqs/SimpleFilterFrequenciesProcessor.java
@@ -2,14 +2,13 @@ package org.toilelibre.libe.soundtransform.infrastructure.service.freqs;
 
 import org.toilelibre.libe.soundtransform.model.exception.SoundTransformException;
 import org.toilelibre.libe.soundtransform.model.freqs.FilterFrequenciesProcessor;
-import org.toilelibre.libe.soundtransform.model.freqs.SurroundInRangeProcessor.SurroundInRangeProcessorErrorCode;
 
 final class SimpleFilterFrequenciesProcessor implements FilterFrequenciesProcessor {
 
     @Override
     public float [] filter (final float [] freqs, final float low, final float high) throws SoundTransformException {
         if (low >= high){
-            throw new SoundTransformException(SurroundInRangeProcessorErrorCode.INVALID_RANGE, new IndexOutOfBoundsException(), low, high);
+            throw new SoundTransformException(FilterFrequenciesProcessorErrorCode.INVALID_RANGE, new IndexOutOfBoundsException(), low, high);
         }
         final float [] result = new float [freqs.length];
         for (int i = 0 ; i < result.length ; i++) {
