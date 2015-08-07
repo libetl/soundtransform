b4aff651aff2f106b44f87200d55908e2558f698
Lionel Benychou <lionel.benychou@sage.com>
2015-08-07 15:10:48 +0200

    Smarter SegmentedChannel browse

--- src/main/java/org/toilelibre/libe/soundtransform/model/converted/sound/SegmentedChannel.java
+++ src/main/java/org/toilelibre/libe/soundtransform/model/converted/sound/SegmentedChannel.java
@@ -27,18 +27,34 @@ public class SegmentedChannel extends Channel {
 
     public static final String THIS_CHANNEL_IS_SEGMENTED_AND_CANNOT_BE_DISPLAYED = "This channel is segmented and cannot be displayed";
     private final List<Sound>  channelParts;
+    private int recentlyViewedIndex = 0;
+    private int recentlyViewedSamplesArrayIndex = 0;
+    private int recentlyViewedSamplesArrayPosition = 0;
+    private int recentlyViewedSamplesLength = 0;
+    private int recentlyViewedSamplesArrayNumber = 0;
 
     public SegmentedChannel (final FormatInfo formatInfo, final List<Sound> sounds) {
         super (null, formatInfo, 0);
         this.channelParts = sounds;
     }
+    
+    private void updateRecentlyViewedChannelLength () {
+
+        int segmentIndex = recentlyViewedSamplesArrayNumber;
+        int numberOfSamples = recentlyViewedSamplesLength;
+        while (segmentIndex < this.channelParts.size ()) {
+            numberOfSamples += this.channelParts.get (segmentIndex).getSamplesLength ();
+            segmentIndex++;
+        }
+        this.recentlyViewedSamplesLength = numberOfSamples;
+        this.recentlyViewedSamplesArrayNumber = segmentIndex;
+    }
 
     @Override
     public long getSampleAt (final int index) {
-        final int channelPartIndex = this.getChannelPartIndex (index);
-        final int samplesIndex = this.getSamplesIndex (index);
+        this.updateRecentlyViewedIndex (index);
 
-        return this.channelParts.get (channelPartIndex).getChannels () [0].getSampleAt (samplesIndex);
+        return this.channelParts.get (this.recentlyViewedSamplesArrayIndex).getChannels () [0].getSampleAt (this.recentlyViewedSamplesArrayPosition);
     }
 
     @Override
@@ -51,67 +67,71 @@ public class SegmentedChannel extends Channel {
         return SegmentedChannel.THIS_CHANNEL_IS_SEGMENTED_AND_CANNOT_BE_DISPLAYED;
     }
 
-    private int getChannelPartIndex (final int index) {
-        int realIndex = index;
-        int channelPartIndex = 0;
-        while (channelPartIndex < this.channelParts.size () && realIndex >= this.channelParts.get (channelPartIndex).getSamplesLength ()) {
-            realIndex -= this.channelParts.get (channelPartIndex).getSamplesLength ();
-            channelPartIndex++;
-        }
-        if (channelPartIndex == this.channelParts.size ()) {
-            throw new SoundTransformRuntimeException (new SoundTransformException (SegmentedChannelErrorCode.ARRAY_INDEX_OUT_OF_BOUNDS, new ArrayIndexOutOfBoundsException (), index));
-        }
-        return channelPartIndex;
-    }
 
-    private int getSamplesIndex (final int index) {
-        int realIndex = index;
-        int channelPartIndex = 0;
-        while (channelPartIndex < this.channelParts.size () && realIndex >= this.channelParts.get (channelPartIndex).getSamplesLength ()) {
-            realIndex -= this.channelParts.get (channelPartIndex).getSamplesLength ();
-            channelPartIndex++;
-        }
-        return realIndex;
+    private void updateRecentlyViewedIndex (int wantedIndex) {
+        
+        int currentGlobalIndex = this.recentlyViewedIndex;
+        int currentIndex = this.recentlyViewedSamplesArrayIndex;
+        int currentPosition = this.recentlyViewedSamplesArrayPosition;
+        
+        while (currentGlobalIndex != wantedIndex) {
+            int delta = (int) Math.signum (wantedIndex - currentGlobalIndex);
+            currentGlobalIndex += delta;
+            currentPosition += delta;
+            
+            if (currentPosition < 0) {
+                currentIndex--;
+                if (currentIndex < 0) {
+                    throw new SoundTransformRuntimeException (new SoundTransformException (SegmentedChannelErrorCode.ARRAY_INDEX_OUT_OF_BOUNDS, new ArrayIndexOutOfBoundsException (), currentIndex));
+                }
+                currentPosition = this.channelParts.get (currentIndex).getSamplesLength () - 1;
+            }
+
+            if (currentPosition >= this.channelParts.get (currentIndex).getSamplesLength ()) {
+                currentIndex++;
+                if (currentIndex >= this.channelParts.size ()) {
+                    throw new SoundTransformRuntimeException (new SoundTransformException (SegmentedChannelErrorCode.ARRAY_INDEX_OUT_OF_BOUNDS, new ArrayIndexOutOfBoundsException (), currentIndex));
+                }
+                currentPosition = 0;
+            }        }
+        
+        this.recentlyViewedIndex = wantedIndex;
+        this.recentlyViewedSamplesArrayIndex = currentIndex;
+        this.recentlyViewedSamplesArrayPosition = currentPosition;
     }
+    
 
     @Override
     public int getSamplesLength () {
-        int segmentIndex = 0;
-        int numberOfSamples = 0;
-        while (segmentIndex < this.channelParts.size ()) {
-            numberOfSamples += this.channelParts.get (segmentIndex).getSamplesLength ();
-            segmentIndex++;
-        }
-        return numberOfSamples;
+        this.updateRecentlyViewedChannelLength ();
+        return this.recentlyViewedSamplesLength;
     }
 
     @Override
-    public void setSampleAt (final int index, final long value) {
-        final int channelPartIndex = this.getChannelPartIndex (index);
-        final int samplesIndex = this.getSamplesIndex (index);
+    public synchronized void setSampleAt (final int index, final long value) {
+        this.updateRecentlyViewedIndex (index);
 
-        this.channelParts.get (channelPartIndex).getChannels () [0].setSampleAt (samplesIndex, value);
+        this.channelParts.get (this.recentlyViewedSamplesArrayIndex).getChannels () [0].setSampleAt (this.recentlyViewedSamplesArrayPosition, value);
     }
 
     @Override
-    public void copyTo (final long [] samples) {
+    public synchronized void copyTo (final long [] samples) {
         this.copyTo (samples, 0, 0, this.getSamplesLength ());
     }
 
     @Override
-    public void copyTo (final Channel channel) {
+    public synchronized void copyTo (final Channel channel) {
         this.copyTo (channel.samples, 0, 0, this.getSamplesLength ());
     }
 
     @Override
-    public void copyTo (final long [] samples, final int srcPos, final int dstPos, final int length) {
+    public synchronized void copyTo (final long [] samples, final int srcPos, final int dstPos, final int length) {
         int dstPos1 = dstPos;
         int remainingLength = length;
-        final int channelPartIndex = this.getChannelPartIndex (srcPos);
-        final int samplesIndex = this.getSamplesIndex (srcPos);
-        int i = channelPartIndex;
+        this.updateRecentlyViewedIndex (srcPos);
+        int i = this.recentlyViewedSamplesArrayIndex;
         while (i < this.channelParts.size () && remainingLength > 0) {
-            this.channelParts.get (i).getChannels () [0].copyTo (samples, i == channelPartIndex ? samplesIndex : 0, dstPos1, Math.min (this.channelParts.get (i).getChannels () [0].getSamplesLength (), remainingLength));
+            this.channelParts.get (i).getChannels () [0].copyTo (samples, i == this.recentlyViewedSamplesArrayIndex ? this.recentlyViewedSamplesArrayPosition : 0, dstPos1, Math.min (this.channelParts.get (i).getChannels () [0].getSamplesLength (), remainingLength));
             dstPos1 += this.channelParts.get (i).getChannels () [0].getSamplesLength ();
             remainingLength -= this.channelParts.get (i).getChannels () [0].getSamplesLength ();
             i++;
@@ -119,7 +139,7 @@ public class SegmentedChannel extends Channel {
     }
 
     @Override
-    public void copyTo (final Channel channel, final int srcPos, final int dstPos, final int length) {
+    public synchronized void copyTo (final Channel channel, final int srcPos, final int dstPos, final int length) {
         this.copyTo (channel.samples, srcPos, dstPos, length);
     }
 
